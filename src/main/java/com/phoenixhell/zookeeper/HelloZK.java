e6eb136bf61afde9097a72b30b48d29668c52d78
phoenixhell <phoenixrever@gmail.com>
2021-01-29 22:42:30 +0800

    2020-01-29

---
+++ src/main/java/com/phoenixhell/zookeeper/HelloZK.java
@@ -0,0 +1,65 @@
+package com.phoenixhell.zookeeper;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.zookeeper.*;
+import org.apache.zookeeper.data.Stat;
+
+import java.io.IOException;
+import java.util.Arrays;
+
+@Slf4j
+public class HelloZK {
+    private static final String CONNECT_STRING = "192.168.1.100:2181";
+    private static final int SESSION_TIMEOUT = 3 * 1000;
+    private static final String NODE_PATH = "/hellodsfd";
+    private static final String NODE_VALUE = "value11";
+
+    public static void main(String[] args) {
+        HelloZK helloZK = null;
+        ZooKeeper zookeeper = null;
+        try {
+            helloZK = new HelloZK();
+            zookeeper = helloZK.getZookeeper();
+            Stat exists = zookeeper.exists(NODE_PATH,false);
+            if(exists==null){
+                helloZK.createZnode(zookeeper, NODE_PATH, NODE_VALUE);
+            }
+            String znodeValue = helloZK.getZnodeValue(zookeeper, NODE_PATH);
+            System.out.println(znodeValue);
+            log.info(znodeValue+"-------------------");
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (helloZK != null) {
+                    helloZK.closeConnection(zookeeper);
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public ZooKeeper getZookeeper() throws IOException {
+        return new ZooKeeper(CONNECT_STRING, SESSION_TIMEOUT, new Watcher() {
+            public void process(WatchedEvent event) {
+
+            }
+        });
+    }
+
+    public void closeConnection(ZooKeeper zooKeeper) throws InterruptedException {
+        if (zooKeeper != null) {
+            zooKeeper.close();
+        }
+    }
+
+    public void createZnode(ZooKeeper zooKeeper, String nodPath, String nodeValue) throws KeeperException, InterruptedException {
+        zooKeeper.create(nodPath, nodeValue.getBytes(), ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT);
+    }
+
+    public String getZnodeValue(ZooKeeper zooKeeper, String nodePath) throws KeeperException, InterruptedException {
+        byte[] data = zooKeeper.getData(nodePath, false, new Stat());
+//        String s=new String(data);
+        return Arrays.toString(data);
+    }
+}
